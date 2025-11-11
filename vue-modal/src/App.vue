c80d122462fb66ce4da5a3d771397f2c11102921
Kalthoum <kalatrash22@gmail.com>
2025-11-11 16:57:54 +0200

    reorganized version + vue

---
+++ vue-modal/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>{{title}}</h1>
+  <input type= "text" ref="name">
+  <button @click="handleClick">Click me</button>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+  data(){
+    return{
+     title: 'My First Vue App ;)'
+    }
+  },
+  methods:{
+    handleClick(){
+    console.log(this.$refs.name)
+    this.$refs.name.classList.add('input-ref')
+    this.$refs.name.focus()
+    }
+  }  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ vue-modal/src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')// it is going to be injected in the div with id app in the index html file 
