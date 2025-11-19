2aa28e8ef33adfa1c9065d6bd2c929c46f1099d7
Kalthoum <kalatrash22@gmail.com>
2025-11-19 16:14:11 +0200

    lesson #7

---
+++ web-form/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <signUp/>  
+</template>
+
+<script>
+
+import signUp from "./components/signup.vue"
+export default {
+  name: 'App',
+  components: {signUp}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  amrgin : 0;
+  background: #eee;
+}
+</style>
